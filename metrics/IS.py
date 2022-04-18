859a17f0f777636741c493330b5df71ae52154bb
Song <quanpengsong@gmail.com>
2022-04-18 17:55:00 +0800

    Inception Score 实现完毕

---
+++ metrics/IS.py
@@ -0,0 +1,79 @@
+# https://github.com/sbarratt/inception-score-pytorch/blob/master/inception_score.py
+import argparse
+
+import numpy as np
+from torchvision.models.inception import inception_v3
+from utils.data import NoLabelDataset
+from torchvision import transforms
+from torch.utils import data
+from tqdm import tqdm
+import torch.nn.functional as F
+from scipy.stats import entropy
+
+
+def inception_score(args):
+    transform = transforms.Compose([
+        transforms.CenterCrop(args.img_size),
+        transforms.Resize(args.img_size),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ])
+    dataset = NoLabelDataset(args.img_path, transform)
+    print(f"Found {len(dataset)} images.")
+    dataloader = data.DataLoader(dataset=dataset, batch_size=args.batch_size, shuffle=False,
+                                 num_workers=args.num_workers, pin_memory=True)
+    preds = []
+    print("Initializing model...")
+    model = inception_v3(pretrained=True)
+    model.eval()
+    print("Processing images...")
+    for images in tqdm(dataloader):
+        images.cuda()
+        local_preds = model(images)
+        local_preds = F.softmax(local_preds, dim=1).data.cpu().numpy()
+        local_preds = [pred for pred in local_preds]
+        preds.extend(local_preds)
+    print("Calculating inception score...")
+    preds = np.array(preds)
+    num_per_split = preds.shape[0] // args.split_num
+    # Method #1: https://gitee.com/songquanpeng/tf-fid-is/blob/master/inception_score.py#L47
+    scores = []
+    for i in range(args.split_num):
+        split = preds[i * num_per_split:(i + 1) * num_per_split, :]
+        # KL(p(y|x)||p(y)) = Sigma_i_to_n[p(y|xi)log(p(y|xi) / p(y))]
+        KL_divergence = split * (np.log(split) - np.log(np.expand_dims(np.mean(split, 0), 0)))
+        KL_divergence = np.mean(np.sum(KL_divergence, 1))
+        # InceptionScore = exp(E(KL))
+        scores.append(np.exp(KL_divergence))
+    IS_mean, IS_std = np.mean(scores), np.std(scores)
+    print(f"Inception Score: {IS_mean:.4f}±{IS_std:.4f}")
+
+    # Method #2: https://github.com/sbarratt/inception-score-pytorch/blob/master/inception_score.py
+    # scores = []
+    # for k in range(args.split_num):
+    #     split = preds[k * num_per_split:(k + 1) * num_per_split, :]
+    #     py = np.mean(split, axis=0)
+    #     local_scores = []
+    #     for i in range(split.shape[0]):
+    #         pyx = split[i, :]
+    #         # actually it's the KL divergence
+    #         KL_divergence = entropy(pyx, py)
+    #         local_scores.append(KL_divergence)
+    #     scores.append(np.exp(np.mean(local_scores)))
+    # IS_mean, IS_std = np.mean(scores), np.std(scores)
+    # print(f"Inception Score: {IS_mean:.4f}±{IS_std:.4f}")
+    return IS_mean, IS_std
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--img_path', type=str)
+    parser.add_argument('--img_size', type=int, default=229,
+                        help='The default input image size of Inception-v3 is 299×299.')
+    parser.add_argument('--num_workers', type=int, default=0)
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--split_num', type=int, default=10)
+    cfg = parser.parse_args()
+    if cfg.img_path is None:
+        cfg.img_path = input("Input the target image path: ").strip('"').strip("'")
+    inception_score(cfg)
--- utils/data.py
+++ utils/data.py
@@ -1,7 +1,8 @@
 import os
-
+import glob
 from torchvision import datasets, transforms
 from torch.utils import data
+from PIL import Image
 
 
 def get_MNIST_loader(img_size=28, batch_size=1, dataset_path='archive/dataset'):
@@ -14,3 +15,30 @@ def get_MNIST_loader(img_size=28, batch_size=1, dataset_path='archive/dataset'):
     train_dataset = datasets.MNIST(root=dataset_path, download=True, train=True, transform=data_transforms)
     dataloader = data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=True)
     return dataloader
+
+
+def list_all_images(path, full_path=True):
+    image_types = ['png', 'jpg', 'jpeg']
+    image_list = []
+    for image_type in image_types:
+        image_list.extend(glob.glob(os.path.join(path, f"**/*.{image_type}"), recursive=True))
+    if not full_path:
+        image_list = [os.path.relpath(image, path) for image in image_list]
+    image_list = [p.replace("\\", '/') for p in image_list]
+    return image_list
+
+
+class NoLabelDataset(data.Dataset):
+    def __init__(self, root, transform):
+        self.samples = list_all_images(root, full_path=True)
+        self.samples.sort()
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.samples[idx])
+        if self.transform:
+            img = self.transform(img)
+        return img
